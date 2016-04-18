d156efd7691b2945bacf5069ea94a60cb04fe533
Frank Desmettre <frank.desmettre@gmail.com>
2016-04-18 08:26:08 +0200

    Return list of friends with common games

--- app/models/user.js
+++ app/models/user.js
@@ -52,44 +52,34 @@ var UserSchema = new Schema({
 		'type': String
 	},
 	avatar: {
-        'type': String
-    },
+		'type': String
+	},
 	library: {
-		platforms: {
-			'items': {
+		platforms: [{
 				'id': {
 					'type': Number
 				}
-			}
-		},
-		games: {
-			'items': {
-				id: {
-					'type': Number
-				},
-				name: {
-					'type': String
-				},
-				platforms: {
-					'items': {
-						'id': {
-							'type': Number
-						}
+		}],
+		games: [{
+			id: {
+				'type': Number
+			},
+			name: {
+				'type': String
+			},
+			platforms: {
+				'items': {
+					'id': {
+						'type': Number
 					}
 				}
 			}
-		}
+		}]
 	},
-	friends: {
-		'items': {
-			'id': {
-				'type': Number
-			},
-            'name': {
-                'type': String
-            }
-		}
-	},
+	friends: [{
+		'id': Number,
+		'name': String
+	}],
 	userData: {
 		registerDate: Date,
 		lastLogon: Date,
@@ -99,11 +89,11 @@ var UserSchema = new Schema({
 });
 
 // hash the password and userID before the user is saved
-UserSchema.pre('save', function(next){
+UserSchema.pre('save', function (next) {
 	var user = this;
 
 	// set userID, registration date, usertype and active when new user
-	if(user.isModified('username')) {
+	if (user.isModified('username')) {
 		var hash = 0, i, char;
 		if (user.username.length == 0) return hash;
 		for (i = 0; i < user.username.length; i++) {
@@ -111,30 +101,30 @@ UserSchema.pre('save', function(next){
 			hash = ((hash << 5) - hash) + char;
 			hash |= 0; // Convert to 32bit integer
 		}
-		user.userID = Math.abs(hash).toString().substring(0,7);
+		user.userID = Math.abs(hash).toString().substring(0, 7);
 		user.userData.registerDate = new Date();
 		user.userData.active = true;
 		user.usertype = 'user';
-		user.gender =  'undefined';
-        user.bio = '';
-        user.library = {};
-        user.library.platforms = [];
-        user.library.games = [];
-        user.friends = [];
+		user.gender = 'undefined';
+		user.bio = '';
+		user.library = {};
+		user.library.platforms = [];
+		user.library.games = [];
+		user.friends = [];
 
 	}
 
 	// hash the password only if the password has been changed or user is new
-	if(!user.isModified('password')) return next();
-	bcrypt.hash(user.password, null, null, function(err, hash){
-		if(err) return next(err);
+	if (!user.isModified('password')) return next();
+	bcrypt.hash(user.password, null, null, function (err, hash) {
+		if (err) return next(err);
 		user.password = hash;
 		next();
 	});
 });
 
 // method to compare a given password with the database hash
-UserSchema.methods.comparePassword = function(password) {
+UserSchema.methods.comparePassword = function (password) {
 	var user = this;
 
 	return bcrypt.compareSync(password, user.password);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,24 +1,50 @@
-var bodyParser  = require('body-parser'), 	// get body-parser
-    User        = require('../models/user'),
-    jwt         = require('jsonwebtoken'),
-    http        = require('http'),
-    config      = require('../config');
+var bodyParser = require('body-parser'), 	// get body-parser
+	User = require('../models/user'),
+	jwt = require('jsonwebtoken'),
+	http = require('http'),
+	config = require('../config'),
+	async = require('async'),
+	_ = require('lodash');
 
 // super secret for creating tokens
 var superSecret = config.secret;
 
-module.exports = function(app, express) {
+module.exports = function (app, express) {
 
-	var apiRouter = express.Router();
+	var apiRouter = new express.Router();
 
 	apiRouter
-        .get('/test', function(req, res){
-			User.find({"username": "demouser"}, function(err, user){
-				for(var key in user[0].library.games){
-					console.log(user[0].library.games[key]);
+		.get('/test', function (req, res) {
+
+
+
+			function getUser(cb) {
+					User.findOne({ "username": "demouser" }, cb);
+			}
+
+			function getGamesOfFriends(user, cb) {
+
+				if (!user.friends) {
+					return res.status(404).send('No friends found');
 				}
+				var friendIds = _.map(user.friends, 'id');
+				var ownGames = _.map(user.library.games, 'name');
+
+				User.find({
+					'userID': { $in: friendIds },
+					'library.games.name': { $in: ownGames }
+				}, cb);
+			}
+
+			async.waterfall([
+				getUser,
+				getGamesOfFriends
+			], function onFinish(err, result) {
+				if (err) return res.status(400).send('Bad request');
+
+				res.json(result);
 			});
-        });
+		});
 
 	return apiRouter;
 };
